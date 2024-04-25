429ec7ac8377ed45b8fc5a65f6fddf8d5f45de2d
Neel Patel <neelpatelbiz@gmail.com>
2024-04-25 13:44:40 +0000

    update lazy box path

--- perf/full_run.sh
+++ perf/full_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
+LBX="$(dirname "$0")/../../lazybox"
 rm -fr ./results
-sudo ~/lazybox/scripts/zram_swap.sh 4G
+sudo $LBX/scripts/zram_swap.sh 4G
 from_date=$(date)
 time CFG=full_once_config.sh ./run.sh
 echo "tests ran from $from_date to $(date)"
