bf2bee996c1210d0c819faee0ebe392bf528f9ed
Neel Patel <neelpatelbiz@gmail.com>
2024-05-02 17:32:15 +0000

    sweep quota ms

---
+++ perf/masim_goals_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+EXPERIMENTS=`dirname $BASH_SOURCE`
+
+masim_configs="stairs 100mb"
+
+workloads=""
+
+for w in $masim_configs
+do
+	workloads+="masim/$w "
+done
+
+vars="orig "
+quota_ms=( 1 10 25 100 1000 )
+for num in ${quota_ms[@]}; do 
+	[ ! -d schemes/prcl_quota_${num}ms.json ] && sed "s/\"time_ms\": \"1 s\"/\"time_ms\": \"$num ms\"/g" schemes/prcl_quota.json > schemes/prcl_quota_${num}ms.json ; 
+	vars+="prcl_quota_${num}ms "
+done 
+
+VARIANTS=""
+
+for v in $vars
+do
+	for w in $workloads
+	do
+		VARIANTS+="$w/$v "
+	done
+done
+
+EXPERIMENTS=$EXPERIMENTS
+VARIANTS=$VARIANTS
+REPEATS=1
