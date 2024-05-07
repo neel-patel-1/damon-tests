aeff412fcb00adb117dcc8c0c19952f51f2ce802
Neel Patel <neelpatelbiz@gmail.com>
2024-05-07 02:26:03 +0000

    target tuning output

--- perf/aggressor_cpu_tuning.sh
+++ perf/aggressor_cpu_tuning.sh
@@ -2,9 +2,16 @@
 
 kdamond_cpu=100
 target_cpu=100
-log_file=convergence_time.txt
 
-echo "Time kdamond target" | tee $log_file
+do_tuning=0
+
+if [ $do_tuning -eq 1 ]; then
+	echo "Time kdamond target" | tee $log_file
+	log_file=feedback-auto-tuning.txt
+else
+	echo "Time kdamond" | tee $log_file
+	log_file=no_auto-tuning.txt
+fi
 
 # Generate values for a certain number of steps
 while [ "1" ] ; do
@@ -14,10 +21,14 @@ while [ "1" ] ; do
 		
 		# Print the value
 		time=$(date +"%s")
-		echo $time $kdamond_cpu $target_cpu | tee -a $log_file
-		echo $target_cpu | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/contexts/0/schemes/0/quotas/goals/0/target_value ;
-		echo $kdamond_cpu | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/contexts/0/schemes/0/quotas/goals/0/current_value;
-		echo commit_schemes_quota_goals | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/state
+		if [ $do_tuning -eq 1 ]; then
+			echo $time $kdamond_cpu $target_cpu | tee -a $log_file
+			echo $target_cpu | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/contexts/0/schemes/0/quotas/goals/0/target_value ;
+			echo $kdamond_cpu | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/contexts/0/schemes/0/quotas/goals/0/current_value;
+			echo commit_schemes_quota_goals | sudo tee /sys/kernel/mm/damon/admin/kdamonds/0/state
+		else
+			echo $time $kdamond_cpu | tee -a $log_file
+		fi
 
 	done
 done
